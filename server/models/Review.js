1dddc9cb8496adbece6d217a33f1f4f97e68391e
[philip iacone] <[philiacone@gmail.com]>
2024-02-10 16:04:09 -0500

    Started models for user and review

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,15 +1,15 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const thoughtSchema = new Schema({
-  thoughtText: {
+const reviewSchema = new Schema({
+  reviewText: {
     type: String,
-    required: 'You need to leave a thought!',
+    required: 'Leave your review',
     minlength: 1,
     maxlength: 280,
     trim: true,
   },
-  thoughtAuthor: {
+  reviewAuthor: {
     type: String,
     required: true,
     trim: true,
@@ -40,6 +40,6 @@ const thoughtSchema = new Schema({
   ],
 });
 
-const Thought = model('Thought', thoughtSchema);
+const Review = model('Review', reviewSchema);
 
-module.exports = Thought;
+module.exports = Review;
