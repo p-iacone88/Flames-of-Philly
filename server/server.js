2b1d43e3554412c6ac9024fa0556bf3dda27be24
[philip iacone] <[philiacone@gmail.com]>
2024-02-12 10:33:30 -0500

    updating packages

--- server/server.js
+++ server/server.js
@@ -35,6 +35,7 @@ const startApolloServer = async () => {
 
     db.once('open', () => {
         app.listen(PORT, () => {
+            // console.log(db);
             console.log(`API server running on port ${PORT}!`);
             console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
         });
