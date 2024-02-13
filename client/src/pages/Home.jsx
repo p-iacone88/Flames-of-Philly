f9dccc1576a1eb2f3e1ce6cb2d011c633782edd5
kennycummings <kennethccummings@gmail.com>
2024-02-13 10:29:21 -0500

    Created Home.css and Home.jsx files

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h2>Welcome to Flames of Philly!</h2>
+    </div>
+  );
+};
+
+export default Home;
