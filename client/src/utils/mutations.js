da58eb2ebd253b7aea86dee8b32e41816c912cfa
kennycummings <kennethccummings@gmail.com>
2024-02-14 11:41:35 -0500

    Added routes and fixed functionality

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ export const ADD_USER = gql`
   }
 `;
 
-export const LOGIN = gql`
+export const LOGIN_USER = gql`
   mutation Login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
